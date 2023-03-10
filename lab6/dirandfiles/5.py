25e1d595077a778d5e417589410ae19070418db8
aibek013 <124352413+aibek013@users.noreply.github.com>
2023-03-11 04:40:03 +0600

    Delete lab6/dirandfiles directory

--- lab6/dirandfiles/5.py
+++
@@ -1,7 +0,0 @@
-file = open('realmadrid.txt', 'w')
-mylist = ['Real Madrid the best team in history', 'i am Nurasyl', 1, 115]
-for i in mylist:
-    file.write(str(i) + '\n')
-file.close()
-f = open('realmadrid.txt', 'r')
-print(f.read())
--- lab6/dirandfiles/8.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path = r'delfile.txt'
-if os.path.exists(path):
-   os.remove(path)
-   print('removed')
-else:
-   print('your file does not exists')
