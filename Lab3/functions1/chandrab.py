810ce9362e7350ac6641fe277bf1e5674de33f9e
aibek013 <muratov.aibek02@mail.ru>
2023-03-10 10:45:00 +0600

    upd

---
+++ Lab3/functions1/chandrab.py
@@ -0,0 +1,4 @@
+def solve(numheads, numlegs):
+   chickens = (numlegs - (4 * numheads)) / -2
+   rabbits = numheads - chickens
+   return int(chickens), int(rabbits)
---
+++ Lab3/functions1/hist.py
@@ -0,0 +1,5 @@
+def histogram(my_list): 
+    for i in my_list:
+        for j in range(i):
+            print("*", end = "")
+        print()
